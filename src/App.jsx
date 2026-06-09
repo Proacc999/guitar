8a433069010d27481bf7e7cbd8b5118dd9ed9bf2
Proaccc999 <Proacc@mynes.com>
2026-06-09 14:17:24 +0530

    for stem

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">App</h1>
- 
+      <h1 className="text-3xl font-bold underline">Liff Demo</h1>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click to open url
+        </button>
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click to open window
+        </button>
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click to open external para stem
+        </button>
+      </div>
     </div>
   )
 }
